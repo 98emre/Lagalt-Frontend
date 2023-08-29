ac177317d92f087c70ab7473a7bca204ee2bff09
August Danell <august.danell@se.experis.com>
2023-08-29 15:51:48 +0200

    Working on forms

--- Lagalt/src/app/app.module.ts
+++ Lagalt/src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProfilePageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- Lagalt/src/app/pages/profile-page/profile-page.component.ts
+++ Lagalt/src/app/pages/profile-page/profile-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-profile-page',
@@ -6,7 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile-page.component.scss']
 })
 export class ProfilePageComponent {
-onClick() {
-  alert("profile-page.onClick() was called");
-}
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  dummyName:String = "Mr Brass"
+  dummyDescription:String = "I work in retail, I am 45 years old but my humor is that of a 15 year old. My motto: Laugh hard. Die Hard."
+  onClickAddProject() {
+    alert("profile-page.onClick() was called");
+  }
+  public onSubmit(form: NgForm): void {
+    console.log("Submitted", form.value)
+  }
 }
