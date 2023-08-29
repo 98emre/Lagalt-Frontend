c7fc4b9a470a18af6481d86f04e455f7cbc7a9bf
August Danell <august.danell@se.experis.com>
2023-08-29 20:25:23 +0200

    Chore: Integrating Project models with project list, inserting data into project list items

--- Lagalt/src/app/pages/profile-page/profile-page.component.ts
+++ Lagalt/src/app/pages/profile-page/profile-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ProjectItemComponent } from 'src/app/components/project-item/project-item.component';
+import { Project } from 'src/app/models/project';
 
 
 @Component({
@@ -12,9 +13,9 @@ export class ProfilePageComponent implements OnInit{
   constructor() { }
   ngOnInit(): void {}
 
-  dummyName:String = "Mr Brass-Bilalsson"
-  dummyDescription:String = "I work in retail, I am 45 years old but my humor is that of a 15 year old. My motto: Laugh hard. Die Hard. Bruce Willys stars in Die Hard. I am him. I am a shining star. On a bright day."
-  projectList:ProjectItemComponent[] = [ProjectItemComponent, ProjectItemComponent, ProjectItemComponent]
+  dummyName:String = "Mr Brass-Bilalsson (Billys)"
+  dummyDescription:String = "I work in retail, I am 45 years old but my humor is that of a 15 year old. My motto: Laugh hard. Die Hard. Bruce Willys stars in Die Hard. I pretend am him. In life i am shining star... but on a bright day (sun in my eye). In gaming I play single player games cause they remind me that in real life I am single player. As project I look for frend i can play with. I dont have skills (sadge). Also I dislike spider because have claustrofobia. thx."
+  projectList:Project[] = [{name: "Project 1"}, {name: "Project 2"}, {name: "Project 3"}]
 
   /** 
    * TODO: Currently this function only registers a button press. It should perhaps navigate the user to the add project component?
