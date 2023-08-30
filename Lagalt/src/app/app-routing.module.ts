4710a0b683f65fde214e2188d00458d1c94e35de
Vendela <vendela.o@hotmail.com>
2023-08-30 13:41:42 +0200

    chore: keycloak  authentication and auth guard implemented

--- Lagalt/src/app/app-routing.module.ts
+++ Lagalt/src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ProfilePageComponent} from './pages/profile-page/profile-page.component';
 import {LandingPageComponent} from './pages/landing-page/landing-page.component';
-//import {authGuard} from '.'
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [ 
   {
   path: 'profile',
   component: ProfilePageComponent,
+  canActivate: [AuthGuard]
 },
 {
   path:'', 
