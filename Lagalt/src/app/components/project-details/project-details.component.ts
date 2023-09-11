11c70b73749799adfde517b87acad6100448ef55
Vendela <vendela.o@hotmail.com>
2023-09-11 10:19:33 +0200

    chore: displaying project owner on project page

--- Lagalt/src/app/components/project-details/project-details.component.ts
+++ Lagalt/src/app/components/project-details/project-details.component.ts
@@ -3,6 +3,7 @@ import { Collaborator } from 'src/app/models/collaborator';
 import { Project } from 'src/app/models/project';
 import { User } from 'src/app/models/user';
 import { CollaboratorService } from 'src/app/services/collaborator-service.service';
+import { UserService } from 'src/app/services/user-service.service';
 
 @Component({
   selector: 'app-project-details',
@@ -12,17 +13,20 @@ import { CollaboratorService } from 'src/app/services/collaborator-service.servi
 export class ProjectDetailsComponent {
 
   @Input() projectDetails: Project | any;
+  @Input() projectOwner: User | any;
 
   user:User|any = null
   collaboratorModels: Collaborator[] = []
 
-  constructor(private collaboratorService: CollaboratorService){}
+  constructor(private collaboratorService: CollaboratorService, private userService: UserService){}
 
   ngOnInit(){
     this.collaboratorService.getCollaborators().subscribe((collaborators) => {this.collaboratorModels = collaborators}) 
 
-    if(localStorage.getItem('user') != null)
+    if(localStorage.getItem('user') != null){
       this.user = JSON.parse(localStorage.getItem('user')!) 
+    }
+
   }
 
   onCollabButton(){
--- Lagalt/src/app/pages/project-page/project-page.component.ts
+++ Lagalt/src/app/pages/project-page/project-page.component.ts
@@ -3,10 +3,8 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 import { ProjectComment } from 'src/app/models/comment';
 import { Project } from 'src/app/models/project';
 import { User } from 'src/app/models/user';
-import { CommentService } from 'src/app/services/comment-service.service';
 import { ProjectService } from 'src/app/services/project-service.service';
 import { UserService } from 'src/app/services/user-service.service';
-import { CollaboratorService } from 'src/app/services/collaborator-service.service';
 import { Collaborator } from 'src/app/models/collaborator';
 
 @Component({
@@ -18,36 +16,24 @@ export class ProjectPageComponent {
 
   project: Project | any = null;
   commentModels: ProjectComment[] = []
-  //user:User|any = null
+  projectOwner: User | any = null;
   collaboratorModels: Collaborator[] = []
   acceptedCollaboratorModels: Collaborator[] = []
 
-  constructor(private projectService:ProjectService, private route: ActivatedRoute){}
+  constructor(private projectService:ProjectService, private route: ActivatedRoute, private userService: UserService){}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params: ParamMap) => {
       const projectId = Number(params.get('id'));
+      this.projectService.getProjectById(projectId).subscribe((project) => {
+        this.project = project 
 
+        this.userService.getUserById(project.userId).subscribe((projectOwner) => {
+          this.projectOwner = projectOwner;
+        })
+      })
 
-      this.projectService.getProjectById(projectId).subscribe((project) => { this.project = project })
     });
 
-    //this.collaboratorService.getCollaborators().subscribe((collaborators) => {this.collaboratorModels = collaborators})
-
-    /*if(localStorage.getItem('user') != null)
-      this.user = JSON.parse(localStorage.getItem('user')!)*/
   }
-
-  /*onCollabButton(){
-    let newCollaborator:Collaborator = 
-    {
-      id: 1,
-      userId: this.user.id,
-      status: "PENDING", 
-      requestDate: new Date(),
-      approvalDate: null,
-      projectId: this.project.id
-    }
-    this.collaboratorService.postCollaborator(newCollaborator)
-  }*/
 }
